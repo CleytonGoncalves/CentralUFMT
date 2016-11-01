df4aca9cd3bad34b01d5c58448576802d23dbacd
CleytonGoncalves <cleyton.cgoncalves@gmail.com>
2016-11-01 01:20:11 -0300

    Drawing Optimizations

--- app/src/main/java/com/cleytongoncalves/centralufmt/ui/schedule/ScheduleAdapter.java
+++ app/src/main/java/com/cleytongoncalves/centralufmt/ui/schedule/ScheduleAdapter.java
@@ -19,6 +19,7 @@ final class ScheduleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 	private final static String TAG = ScheduleAdapter.class.getSimpleName();
 	private static final int TYPE_HEADER = 0;
 	private static final int TYPE_ITEM = 1;
+	private static final int TYPE_EMPTY = 2;
 	private static final String[] shortDayName = {"seg", "ter", "qua", "qui", "sex", "sáb",
 	                                              "dom"};
 
@@ -36,9 +37,11 @@ final class ScheduleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
 		if (viewType == TYPE_HEADER) {
 			return new HeaderViewHolder(inflater.inflate(R.layout.schedule_header, parent, false));
+		} else if (viewType == TYPE_ITEM) {
+			return new ClassViewHolder(inflater.inflate(R.layout.schedule_item, parent, false));
 		}
 
-		return new ClassViewHolder(inflater.inflate(R.layout.schedule_item, parent, false));
+		return new EmptyViewHolder(inflater.inflate(R.layout.schedule_item_empty, parent, false));
 	}
 
 	@Override
@@ -46,31 +49,32 @@ final class ScheduleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 		if (mSchedulePresenter.isHeader(position)) {
 			((HeaderViewHolder) holder).header.setText(shortDayName[position]);
 			return;
+		} else if (isEmpty(position)) {
+			((EmptyViewHolder) holder).view.setBackgroundColor(Color.parseColor("#f9f9f9"));
+			return;
 		}
 
 		ScheduleItemData data = mSchedulePresenter.getDataForPosition(position);
 		ClassViewHolder classViewHolder = (ClassViewHolder) holder;
 
-		if (data.getTitle().isEmpty()) {
-			classViewHolder.classLayout.setBackgroundColor(Color.parseColor("#f9f9f9"));
+		classViewHolder.classLayout.setBackgroundColor(Color.parseColor("#3370ff"));
+		classViewHolder.classTitleView.setText(data.getTitle());
+		classViewHolder.classScheduleView.setText(data.getSchedule());
+
+		if (data.getRoom().isEmpty()) {
+			classViewHolder.classRoomView.setVisibility(View.INVISIBLE);
 		} else {
-			classViewHolder.classTitleView.setText(data.getTitle());
-			classViewHolder.classLayout.setBackgroundColor(Color.parseColor("#3370ff"));
-
-			if (data.getRoom().isEmpty()) {
-				classViewHolder.classRoomView.setVisibility(View.GONE);
-			} else {
-				classViewHolder.classRoomView.setVisibility(View.VISIBLE);
-				classViewHolder.classRoomView.setText(data.getRoom());
-			}
+			classViewHolder.classRoomView.setVisibility(View.VISIBLE);
+			classViewHolder.classRoomView.setText(data.getRoom());
 		}
-
 	}
 
 	@Override
 	public int getItemViewType(int position) {
 		if (mSchedulePresenter.isHeader(position)) {
 			return TYPE_HEADER;
+		} else if (isEmpty(position)) {
+			return TYPE_EMPTY;
 		}
 
 		return TYPE_ITEM;
@@ -84,6 +88,10 @@ final class ScheduleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 	@Override
 	public int getItemCount() {
 		return mSchedulePresenter.getItemCount();
+	}
+
+	private boolean isEmpty(int position) {
+		return mSchedulePresenter.getDataForPosition(position).getTitle().isEmpty();
 	}
 
 	static class ClassViewHolder extends RecyclerView.ViewHolder {
@@ -102,6 +110,15 @@ final class ScheduleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 		@BindView(R.id.header_schedule_item) TextView header;
 
 		HeaderViewHolder(View itemView) {
+			super(itemView);
+			ButterKnife.bind(this, itemView);
+		}
+	}
+
+	static class EmptyViewHolder extends RecyclerView.ViewHolder {
+		@BindView(R.id.class_empty) View view;
+
+		EmptyViewHolder(View itemView) {
 			super(itemView);
 			ButterKnife.bind(this, itemView);
 		}
