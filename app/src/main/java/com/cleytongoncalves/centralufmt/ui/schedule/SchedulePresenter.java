d3832add376df8c5fb74288c012f03f7ac53d13c
CleytonGoncalves <cleyton.cgoncalves@gmail.com>
2016-11-02 18:51:11 -0300

    TreeMap to SparseArray

--- app/src/main/java/com/cleytongoncalves/centralufmt/ui/schedule/SchedulePresenter.java
+++ app/src/main/java/com/cleytongoncalves/centralufmt/ui/schedule/SchedulePresenter.java
@@ -1,21 +1,21 @@
 package com.cleytongoncalves.centralufmt.ui.schedule;
 
 import android.support.annotation.Nullable;
+import android.util.SparseArray;
 
 import com.cleytongoncalves.centralufmt.data.DataManager;
 import com.cleytongoncalves.centralufmt.data.local.HtmlHelper;
 import com.cleytongoncalves.centralufmt.data.local.PreferencesHelper;
 import com.cleytongoncalves.centralufmt.data.model.Discipline;
 import com.cleytongoncalves.centralufmt.ui.base.Presenter;
+import com.cleytongoncalves.centralufmt.util.TextUtil;
 
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.SortedSet;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 import javax.inject.Inject;
@@ -24,6 +24,7 @@ final class SchedulePresenter implements Presenter<ScheduleMvpView>, ScheduleDat
 	private final static String TAG = SchedulePresenter.class.getSimpleName();
 	private final static int MINIMUM_DAYS_AMOUNT = 5;
 	private final static int MAXIMUM_TITLE_LENGTH = 25;
+	private final static int MAXIMUM_ROOM_LENGTH = 10;
 
 	private final DataManager mDataManager;
 	@Nullable private ScheduleMvpView mScheduleView;
@@ -51,8 +52,9 @@ final class SchedulePresenter implements Presenter<ScheduleMvpView>, ScheduleDat
 					                                                                      ());
 			//disciplineList = mDataManager.getStudent().getCourse().getEnrolledDisciplines();
 
-			Map<Integer, SortedSet<ScheduleItemData>> hourlySchedule = parseRawSchedule
-					                                                           (disciplineList);
+			SparseArray<SortedSet<ScheduleItemData>> hourlySchedule =
+					parseRawSchedule(disciplineList);
+
 			schedule = parseSchuleForAdapter(hourlySchedule);
 			prefHelper.putSchedule(schedule);
 		}
@@ -122,12 +124,14 @@ final class SchedulePresenter implements Presenter<ScheduleMvpView>, ScheduleDat
 
 	/* Private Helper Methods */
 
-	private Map<Integer, SortedSet<ScheduleItemData>> parseRawSchedule(List<Discipline> discList) {
-		Map<Integer, SortedSet<ScheduleItemData>> hourlySchedule = new TreeMap<>();
+	private SparseArray<SortedSet<ScheduleItemData>> parseRawSchedule(List<Discipline> discList) {
+		SparseArray<SortedSet<ScheduleItemData>> hourlySchedule =
+				new SparseArray<>(discList.size()); //Max # of different hours
 
-		for (int i = 0, disciplineListSize = discList.size(); i < disciplineListSize; i++) {
+		for (int i = 0, discListSize = discList.size(); i < discListSize; i++) {
 			Discipline disc = discList.get(i);
 			List<Interval> classTimes = disc.getClassTimes();
+
 			for (int j = 0, classTimesSize = classTimes.size(); j < classTimesSize; j++) {
 				Interval inter = classTimes.get(j);
 				DateTime start = inter.getStart();
@@ -140,13 +144,16 @@ final class SchedulePresenter implements Presenter<ScheduleMvpView>, ScheduleDat
 				}
 
 				String time = start.hourOfDay().getAsText() + ":" +
-						                   start.minuteOfHour().getAsText() + " - " +
-						                   end.hourOfDay().getAsText() + ":" +
-						                   end.minuteOfHour().getAsText();
+						              start.minuteOfHour().getAsText() + " - " +
+						              end.hourOfDay().getAsText() + ":" +
+						              end.minuteOfHour().getAsText();
 
-				String title = formatTitle(disc.getTitle());
-				ScheduleItemData discData = new ScheduleItemData(dayOfWeek - 1, title, time,
-						                                                disc.getRoom());
+				String title = TextUtil.capsMeaningfulWords(disc.getTitle());
+				title = TextUtil.ellipsizeString(title, MAXIMUM_TITLE_LENGTH);
+
+				String room = TextUtil.ellipsizeString(disc.getRoom(), MAXIMUM_ROOM_LENGTH);
+
+				ScheduleItemData discData = new ScheduleItemData(dayOfWeek - 1, title, time, room);
 
 				SortedSet<ScheduleItemData> viewSet = hourlySchedule.get(start.getHourOfDay());
 				if (viewSet == null) {
@@ -161,13 +168,13 @@ final class SchedulePresenter implements Presenter<ScheduleMvpView>, ScheduleDat
 		return hourlySchedule;
 	}
 
-	private List<ScheduleItemData> parseSchuleForAdapter(Map<Integer, SortedSet<ScheduleItemData>>
+	private List<ScheduleItemData> parseSchuleForAdapter(SparseArray<SortedSet<ScheduleItemData>>
 			                                                     hourlySchedule) {
 		List<ScheduleItemData> list = new ArrayList<>(mDaysOfWeekAmount * mMaxDailyClass);
 
-		int position = 0;
-		for (Integer hourRow : hourlySchedule.keySet()) {
-			SortedSet<ScheduleItemData> hourlyData = hourlySchedule.get(hourRow);
+		int scheduleSize = hourlySchedule.size();
+		for (int i = 0, position = 0; i < scheduleSize; i++) {
+			SortedSet<ScheduleItemData> hourlyData = hourlySchedule.valueAt(i);
 
 			for (ScheduleItemData data : hourlyData) {
 				int currDay = position % mDaysOfWeekAmount;
@@ -186,15 +193,6 @@ final class SchedulePresenter implements Presenter<ScheduleMvpView>, ScheduleDat
 		}
 
 		return list;
-	}
-
-	private String formatTitle(String title) {
-		if (title.length() > MAXIMUM_TITLE_LENGTH) {
-			final char ellipse = '\u2026'; //Even though it is only 1 char, it occupies ~2 spaces
-			title = title.substring(0, MAXIMUM_TITLE_LENGTH - 2) + ellipse;
-		}
-
-		return title;
 	}
 
 	private int getItemPosition(int position) {
