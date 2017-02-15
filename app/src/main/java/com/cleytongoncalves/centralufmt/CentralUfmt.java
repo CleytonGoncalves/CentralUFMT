d06f788b826ffbcc679bdaa58914a56925a412d3
CleytonGoncalves <cleyton.cgoncalves@gmail.com>
2017-02-15 00:47:04 -0300

    Moved inner anonymous logger classes to a util Logger class

--- app/src/main/java/com/cleytongoncalves/centralufmt/CentralUfmt.java
+++ app/src/main/java/com/cleytongoncalves/centralufmt/CentralUfmt.java
@@ -2,14 +2,12 @@ package com.cleytongoncalves.centralufmt;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.bugsnag.android.Bugsnag;
-import com.bugsnag.android.Severity;
 import com.cleytongoncalves.centralufmt.injection.component.ApplicationComponent;
 import com.cleytongoncalves.centralufmt.injection.component.DaggerApplicationComponent;
 import com.cleytongoncalves.centralufmt.injection.module.ApplicationModule;
+import com.cleytongoncalves.centralufmt.util.Logger;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
@@ -62,32 +60,10 @@ public class CentralUfmt extends Application {
 
 	private void plantTrees() {
 		if (BuildConfig.DEBUG) {
-			Timber.plant(new Timber.DebugTree() {
-				@Override
-				protected void log(int priority, String tag, String message, Throwable t) {
-					super.log(priority, "Timber_" + tag, message, t);
-				}
-			});
+			Timber.plant(Logger.getDebugTree());
 		} else {
 			Bugsnag.init(this);
-			Timber.plant(new Timber.Tree() {
-				@Override
-				protected void log(int priority, @Nullable String tag, @Nullable String message,
-				                   @Nullable Throwable t) {
-					if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
-						return;
-					}
-
-					Severity severity = (priority == Log.WARN) ? Severity.WARNING : Severity.ERROR;
-					if (t == null) {
-						Bugsnag.notify("Timber_" + tag, message, new StackTraceElement[0],
-						               severity,
-						               null);
-					} else {
-						Bugsnag.notify(t, severity);
-					}
-				}
-			});
+			Timber.plant(Logger.getBugsnagTree());
 		}
 	}
 
---
+++ app/src/main/java/com/cleytongoncalves/centralufmt/util/Logger.java
@@ -0,0 +1,80 @@
+package com.cleytongoncalves.centralufmt.util;
+
+
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.birbit.android.jobqueue.log.CustomLogger;
+import com.bugsnag.android.Bugsnag;
+import com.bugsnag.android.Severity;
+import com.cleytongoncalves.centralufmt.data.jobs.JobExitingException;
+
+import timber.log.Timber;
+import timber.log.Timber.DebugTree;
+import timber.log.Timber.Tree;
+
+public final class Logger {
+	private Logger() {
+	}
+	
+	public static Tree getDebugTree() {
+		return new DebugTree() {
+			@Override
+			protected void log(int priority, String tag, String message, Throwable t) {
+				super.log(priority, "Timber_" + tag, message, t);
+			}
+		};
+	}
+	
+	public static Tree getBugsnagTree() {
+		return new Timber.Tree() {
+			@Override
+			protected void log(int priority, @Nullable String tag, @Nullable String message,
+			                   @Nullable Throwable t) {
+				if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+					return;
+				}
+				
+				Severity severity = (priority == Log.WARN) ? Severity.WARNING : Severity.ERROR;
+				if (t == null) {
+					Bugsnag.notify("Timber_" + tag, message, new StackTraceElement[0],
+					               severity,
+					               null);
+				} else {
+					Bugsnag.notify(t, severity);
+				}
+			}
+		};
+	}
+	
+	public static CustomLogger getJobQueueLogger() {
+		return new CustomLogger() {
+			@Override
+			public boolean isDebugEnabled() {
+				return false;
+			}
+			
+			@Override
+			public void d(String text, Object... args) {
+				//Timber.d(text, args);
+			}
+			
+			@Override
+			public void e(Throwable t, String text, Object... args) {
+				//prevents from printing an error on expected Job exceptions
+				if (t instanceof JobExitingException) { return; }
+				
+				Timber.e(t, text, args);
+			}
+			
+			@Override
+			public void e(String text, Object... args) {
+				Timber.e(text, args);
+			}
+			
+			@Override
+			public void v(String text, Object... args) {
+			}
+		};
+	}
+}
